0088c93e94e0e155f9555bac6b95b3c0d2b08cb8
Gabriel Miranda <gabrielmfern@outlook.com>
2025-11-11 17:24:57 -0300

    chore: renovate (#36)
    * remove excess dependencies, update npm
    
    * update dependencies
    
    * remove eslint config
    
    * remove lock
    
    * use a simple API route instead of rendering in the page
    
    * add missing transactional dependencies
    
    * remove unecessary react import
    
    * format

---
+++ apps/web/app/api/send.tsx
@@ -0,0 +1,14 @@
+"use server";
+import { render } from "@react-email/components";
+import { VercelInviteUserEmail } from "transactional/emails/vercel-invite-user";
+
+export async function POST(username: string) {
+	const emailHtml = await render(<VercelInviteUserEmail username={username} />);
+
+	return new Response(emailHtml, {
+		status: 200,
+		headers: {
+			"Content-Type": "application/html",
+		},
+	});
+}
