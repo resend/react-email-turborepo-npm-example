690e19d5f0d61bfef775e2ac31956c9fd74b9d88
gabriel miranda <gabrielmfern@outlook.com>
2025-11-11 17:17:17 -0300

    format

--- apps/web/app/api/send.tsx
+++ apps/web/app/api/send.tsx
@@ -1,14 +1,14 @@
-'use server'
-import { render } from '@react-email/components';
-import { VercelInviteUserEmail } from 'transactional/emails/vercel-invite-user';
+"use server";
+import { render } from "@react-email/components";
+import { VercelInviteUserEmail } from "transactional/emails/vercel-invite-user";
 
 export async function POST(username: string) {
-  const emailHtml = await render(<VercelInviteUserEmail username={username} />);
+	const emailHtml = await render(<VercelInviteUserEmail username={username} />);
 
-  return new Response(emailHtml, {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/html'
-    }
-  });
+	return new Response(emailHtml, {
+		status: 200,
+		headers: {
+			"Content-Type": "application/html",
+		},
+	});
 }
