48b1373e5735f95ac3013c7efa3e7e3e8d46df2f
Diel Duarte <magdiel.kesller@gmail.com>
2026-04-30 16:45:28 -0300

    chore: renovate (#41)
    * chore: update turbo syntax
    * chore: update react-email

--- apps/web/app/api/send/route.tsx
+++ apps/web/app/api/send/route.tsx
@@ -1,8 +1,9 @@
-"use server";
-import { render } from "@react-email/components";
+import { render } from "react-email";
 import { VercelInviteUserEmail } from "transactional/emails/vercel-invite-user";
 
-export async function POST(username: string) {
+export async function POST(request: Request) {
+	const { username } = await request.json();
+
 	const emailHtml = await render(<VercelInviteUserEmail username={username} />);
 
 	return new Response(emailHtml, {
